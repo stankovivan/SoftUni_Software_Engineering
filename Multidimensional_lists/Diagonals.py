156e8b6461079c4ae41b268e2c9ffd052f21c9be
stankovivan <140972935+stankovivan@users.noreply.github.com>
2023-08-09 20:04:24 +0300

    Add files via upload

---
+++ Multidimensional_lists/Diagonals.py
@@ -0,0 +1,11 @@
+rows = int(input())
+matrix = [input().split(", ") for i in range(rows)]
+sum_diagonals = []
+second = []
+
+for index in range(rows):
+    sum_diagonals.append(matrix[index][index])
+    second.append(matrix[index][rows - index - 1])
+
+print(f"Primary diagonal: {', '.join(sum_diagonals)}. Sum: {sum(int(x) for x in sum_diagonals)}")
+print(f"Secondary diagonal: {', '.join(second)}. Sum: {sum(int(x) for x in second)}")
