9d742393b6b36a3dfc114fccde8e6801fb056a5b
stankovivan <140972935+stankovivan@users.noreply.github.com>
2023-08-20 19:39:56 +0300

    Add files via upload

---
+++ Functions_advanced/demo.py
@@ -0,0 +1,24 @@
+def operate(operation, *args):
+    def add(*args):
+        return sum(y for y in args)
+    def subtract(x, *args):
+        return x - sum(y for y in args)
+    def multiply(*args):
+        result = 1
+        for values in args:
+            result *= values
+        return result
+    def devide(x, *args):
+        result = x
+        for values in args:
+            result /= values
+        return result
+    if operation == '+':
+        return add(*args)
+    elif operation == '-':
+        return subtract(*args)
+    elif operation == '*':
+        return multiply(*args)
+    elif operation == '/':
+        return devide(*args)
+print(operate("+", 1, 2, 3))
